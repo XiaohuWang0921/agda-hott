62a3cb55f9fe4f85793d462f2fcb30e6d448bd24
Xiaohu Wang <xiaohu.wang0921@gmail.com>
2023-03-14 16:44:29 +0100

    extractRoot

--- Creagda.hs
+++ Creagda.hs
@@ -4,3 +4,25 @@ printHelp :: IO ()
 printHelp = do
     progName <- getProgName
     putStrLn $ "Usage: " ++ progName ++ " <module> [comment]"
+
+extractRoot :: String -> Maybe String
+extractRoot s = end . init <$> (include (init s) >>= colon . init)
+    where
+        init :: String -> String
+        init (' ' : s) = init s
+        init s = s
+
+        include :: String -> Maybe String
+        include ('i':'n':'c':'l':'u':'d':'e':s) = Just s
+        include _ = Nothing
+
+        colon :: String -> Maybe String
+        colon (':' : s) = Just s
+        colon _ = Nothing
+
+        end :: String -> String
+        end [] = []
+        end (' ' : s) = case end s of
+            [] -> []
+            t -> ' ' : t
+        end (c : s) = c : end s
