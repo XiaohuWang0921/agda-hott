e467feae1ae010520b31ba047761e074d2a1b29a
Xiaohu Wang <xiaohu.wang0921@gmail.com>
2023-03-17 15:44:52 +0100

    findRootFromDirectory

--- Creagda.hs
+++ Creagda.hs
@@ -1,4 +1,11 @@
 import System.Environment (getProgName)
+import Data.Functor.Compose (Compose (Compose, getCompose))
+import Data.Traversable (for)
+import System.Directory (getDirectoryContents, getCurrentDirectory)
+import System.FilePath (joinPath, normalise, (</>), splitDirectories, isDrive, takeDirectory, dropTrailingPathSeparator)
+import System.IO (Handle, hGetLine, hIsEOF, withFile, IOMode (ReadMode))
+import Control.Monad (join)
+import Control.Applicative
 
 printHelp :: IO ()
 printHelp = do
@@ -26,3 +33,26 @@ extractRoot s = end . init <$> (include (init s) >>= colon . init)
             [] -> []
             t -> ' ' : t
         end (c : s) = c : end s
+
+isAgdaLib :: String -> Bool
+isAgdaLib ".agda-lib" = True
+isAgdaLib (_ : s) = isAgdaLib s
+isAgdaLib [] = False
+
+findRelativeRootFromAgdaLib :: Handle -> IO (Maybe FilePath)
+findRelativeRootFromAgdaLib hdl = do
+    eof <- hIsEOF hdl
+    if eof then return Nothing else do
+        line <- hGetLine hdl
+        case extractRoot line of
+            Just path -> do
+                return $ Just path
+            Nothing -> findRelativeRootFromAgdaLib hdl
+
+findRootFromDirectory :: FilePath -> IO (Maybe FilePath)
+findRootFromDirectory p = do
+    agdaLibs <- ((p </>) <$>) . filter isAgdaLib <$> getDirectoryContents p
+    root <- foldr (liftA2 (<|>)) (return Nothing) $ (\ lib -> withFile lib ReadMode findRelativeRootFromAgdaLib) <$> agdaLibs
+    case root of
+        Nothing -> if isDrive p then return Nothing else findRootFromDirectory $ takeDirectory p
+        Just rp -> return $ Just $ dropTrailingPathSeparator $ normalise $ p </> rp
