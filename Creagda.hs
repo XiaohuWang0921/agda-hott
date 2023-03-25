44b4572086dc09799ca8ef65a1a906839f0cd5ef
Xiaohu Wang <xiaohu.wang0921@gmail.com>
2023-03-25 15:11:49 +0100

    Separate code involving finding root directory

--- Creagda.hs
+++ Creagda.hs
@@ -1,62 +1,14 @@
 import System.Environment (getProgName, getArgs)
-import Data.Functor.Compose (Compose (Compose, getCompose))
-import Data.Traversable (for)
-import System.Directory (listDirectory, getCurrentDirectory, createDirectoryIfMissing)
-import System.FilePath (joinPath, normalise, (</>), splitDirectories, isDrive, takeDirectory, pathSeparator, makeRelative, (<.>))
-import System.IO (Handle, hGetLine, hIsEOF, withFile, IOMode (ReadMode, WriteMode), hPutStr)
-import Control.Monad (join)
-import Control.Applicative (Applicative (liftA2), Alternative ((<|>)))
+import System.Directory (getCurrentDirectory, createDirectoryIfMissing)
+import System.FilePath (normalise, (</>), takeDirectory, pathSeparator, makeRelative, (<.>))
+import System.IO (withFile, IOMode (WriteMode), hPutStr)
 import System.Exit (die)
+import Root (findRootFromDirectory)
 
 printHelpAndExit :: IO a
 printHelpAndExit = do
     progName <- getProgName
     die $ "Usage: " ++ progName ++ " <module> [comment]"
-
-extractRoot :: String -> Maybe String
-extractRoot s = end . init <$> (include (init s) >>= colon . init)
-    where
-        init :: String -> String
-        init (' ' : s) = init s
-        init s = s
-
-        include :: String -> Maybe String
-        include ('i':'n':'c':'l':'u':'d':'e':s) = Just s
-        include _ = Nothing
-
-        colon :: String -> Maybe String
-        colon (':' : s) = Just s
-        colon _ = Nothing
-
-        end :: String -> String
-        end [] = []
-        end (' ' : s) = case end s of
-            [] -> []
-            t -> ' ' : t
-        end (c : s) = c : end s
-
-isAgdaLib :: String -> Bool
-isAgdaLib ".agda-lib" = True
-isAgdaLib (_ : s) = isAgdaLib s
-isAgdaLib [] = False
-
-findRootFromAgdaLib :: Handle -> IO (Maybe FilePath)
-findRootFromAgdaLib hdl = do
-    eof <- hIsEOF hdl
-    if eof then return Nothing else do
-        line <- hGetLine hdl
-        case extractRoot line of
-            Just path -> do
-                return $ Just path
-            Nothing -> findRootFromAgdaLib hdl
-
-findRootFromDirectory :: FilePath -> IO (Maybe FilePath)
-findRootFromDirectory p = do
-    agdaLibs <- ((p </>) <$>) . filter isAgdaLib <$> listDirectory p
-    root <- foldr (liftA2 (<|>)) (return Nothing) $ (\ lib -> withFile lib ReadMode findRootFromAgdaLib) <$> agdaLibs
-    case root of
-        Nothing -> if isDrive p then return Nothing else findRootFromDirectory $ takeDirectory p
-        Just rp -> return $ Just $ normalise $ p </> rp
 
 replace :: (Eq a, Functor f) => a -> a -> f a -> f a
 replace old new = fmap (\ x -> if x == old then new else x)
---
+++ Root.hs
@@ -0,0 +1,51 @@
+module Root where
+
+import System.IO (Handle, hIsEOF, hGetLine, withFile, IOMode(ReadMode))
+import System.FilePath (FilePath, (</>), isDrive, takeDirectory, normalise)
+import Control.Applicative (liftA2, (<|>))
+import System.Directory (listDirectory)
+
+extractRoot :: String -> Maybe String
+extractRoot s = end . init <$> (include (init s) >>= colon . init)
+    where
+        init :: String -> String
+        init (' ' : s) = init s
+        init s = s
+
+        include :: String -> Maybe String
+        include ('i':'n':'c':'l':'u':'d':'e':s) = Just s
+        include _ = Nothing
+
+        colon :: String -> Maybe String
+        colon (':' : s) = Just s
+        colon _ = Nothing
+
+        end :: String -> String
+        end [] = []
+        end (' ' : s) = case end s of
+            [] -> []
+            t -> ' ' : t
+        end (c : s) = c : end s
+
+isAgdaLib :: String -> Bool
+isAgdaLib ".agda-lib" = True
+isAgdaLib (_ : s) = isAgdaLib s
+isAgdaLib [] = False
+
+findRootFromAgdaLib :: Handle -> IO (Maybe FilePath)
+findRootFromAgdaLib hdl = do
+    eof <- hIsEOF hdl
+    if eof then return Nothing else do
+        line <- hGetLine hdl
+        case extractRoot line of
+            Just path -> do
+                return $ Just path
+            Nothing -> findRootFromAgdaLib hdl
+
+findRootFromDirectory :: FilePath -> IO (Maybe FilePath)
+findRootFromDirectory p = do
+    agdaLibs <- ((p </>) <$>) . filter isAgdaLib <$> listDirectory p
+    root <- foldr (liftA2 (<|>)) (return Nothing) $ (\ lib -> withFile lib ReadMode findRootFromAgdaLib) <$> agdaLibs
+    case root of
+        Nothing -> if isDrive p then return Nothing else findRootFromDirectory $ takeDirectory p
+        Just rp -> return $ Just $ normalise $ p </> rp
